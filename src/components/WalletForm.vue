5cdc3f5ec4c8f780a5765c9539c4f9b0bc39908e
Mario Ferron <mario.ferron1997@gmail.com>
2025-03-12 19:05:22 -0700

    Create a WalletForm and WalletView

---
+++ src/components/WalletForm.vue
@@ -0,0 +1,74 @@
+<template>
+    <div style="width: 100%; max-width: 450px;">
+      <h1 class="text-3xl font-bold mb-4">Wallet</h1>
+      <h2 class="text-lg mb-6">Add a new crypto</h2>
+  
+      <form @submit.prevent="submitCrypto" style="background-color: black; border-radius: 10px; border: 1px solid gray; padding: 16px;">
+        <label class="block mb-2 font-semibold">Select cryptocurrency:</label>
+        <select v-model="newCrypto.id" required style="width: 100%; padding: 8px; background: dimgrey; color: white; border: 1px solid gray; border-radius: 5px; margin-bottom: 16px;">
+          <option v-for="crypto in cryptoPricesStore.currentPrices" :key="crypto.id" :value="crypto.id">
+            {{ crypto.name }} ({{ crypto.symbol }})
+          </option>
+        </select>
+  
+        <label class="block mb-2 font-semibold">Amount:</label>
+        <input type="number" v-model="newCrypto.amount" min="0.01" step="0.01" required 
+          style="width: 100%; padding: 8px; background: dimgrey; color: white; border: 1px solid gray; border-radius: 5px; margin-bottom: 16px;" />
+  
+        <label class="block mb-2 font-semibold">Purchase price:</label>
+        <input type="number" v-model="newCrypto.price" min="0.01" step="0.01" required 
+          style="width: 100%; padding: 8px; background: dimgrey; color: white; border: 1px solid gray; border-radius: 5px; margin-bottom: 16px;"/>
+  
+        <label class="block mb-2 font-semibold">Notes:</label>
+        <textarea v-model="newCrypto.notes" style="width: 100%; padding: 8px; background: dimgrey; color: white; border: 1px solid gray; border-radius: 5px; resize: none;" class="mb-4"></textarea>
+  
+        <button class="w-full mt-2 p-3 bg-yellow-500 text-black font-bold rounded-lg hover:bg-yellow-400">
+          Add
+        </button>
+      </form>
+    </div>
+  </template>
+
+<script>
+export default {
+  props: ['cryptoPricesStore'],
+  data(){
+    return{ 
+      newCrypto:{ id: '', amount: 0, price: 0, notes: '' }
+    }
+  },
+  methods: {
+    submitCrypto() {
+      if (!this.newCrypto.id || this.newCrypto.amount <= 0 || this.newCrypto.price <= 0) {
+          alert('Please fill in all fields correctly.');
+          return;
+      }
+
+      let cryptoName = this.newCrypto.id;
+      let cryptoSymbol = this.newCrypto.id;
+      for (const crypto of this.cryptoPricesStore.currentPrices) {
+          if (crypto.id === this.newCrypto.id) {
+          cryptoName = crypto.name;
+          cryptoSymbol = crypto.symbol;
+          break;
+          }
+      }
+
+      this.$emit('crypto-added', {
+          id: this.newCrypto.id,
+          name: cryptoName,
+          symbol: cryptoSymbol,
+          amount: this.newCrypto.amount,
+          price: this.newCrypto.price,
+          notes: this.newCrypto.notes,
+          date: new Date().toLocaleDateString()
+      });
+      this.newCrypto = { id: '', amount: 0, price: 0, notes: '' };
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
--- src/views/authenticated/WalletView.vue
+++ src/views/authenticated/WalletView.vue
@@ -1,20 +1,183 @@
-<script setup lang="ts">
-// TODO: Mario - Call the cryptoPricesStore to retrieve the crypto data (take a look at the HomeView.vue for the example)
-// TODO: Mario - Create the logic to retrieve the user crypto data from the local storage
-// TODO: Mario - Create a logic to save the user crypto data to the local storage
-// TODO: Mario - Create a logic to calculate the total value of the user crypto data based on the current price of the crypto and the price of the crypto when the user added it to the wallet
+<script>
+import WalletForm from '@/components/WalletForm.vue';
+import { WalletService } from '@/services/wallet';
+import { useCryptoPricesStore } from '@/stores/crypto';
+
+export default {
+  components: {
+    WalletForm
+  },
+
+  data() {
+    return {
+      walletService: new WalletService(),
+      cryptoPricesStore: useCryptoPricesStore(),
+      userId: 'user',
+      userCryptoData: [],
+      totalMarketValue: 0,
+      totalTransactionValue: 0,
+      totalWallet: 0,
+      cryptoPricesObject: {}
+    }
+  },
+
+  methods: {
+    loadUserCryptoData() {
+      const data = this.walletService.getUserCryptoData(this.userId)
+      this.userCryptoData = data
+      this.updateCryptoPricesObject()
+      this.calculateTotalMarketValue()
+      this.calculateTotalTransactionValue()
+    },
+
+    updateCryptoPricesObject() {
+      const pricesObject = {};
+      for (const crypto of this.cryptoPricesStore.currentPrices) {
+        pricesObject[crypto.id] = crypto.current_price;
+      }
+      this.cryptoPricesObject = pricesObject;
+    },
+
+    calculateTotalTransactionValue() {
+      let total = 0;
+      
+      for (const userCrypto of this.userCryptoData) {
+        total += userCrypto.amount * userCrypto.price;
+      }
+      this.totalTransactionValue = total;
+    },
+
+    calculateTotalMarketValue() {
+      let total = 0;
+      for (const userCrypto of this.userCryptoData) {
+        const currentPrice = this.cryptoPricesObject[userCrypto.id];
+
+        if (currentPrice) {
+          total += userCrypto.amount * currentPrice;
+        }
+      }
+      this.totalMarketValue = total;
+    },
+
+    calculateTotalWallet() {
+      this.totalWallet = this.totalMarketValue - this.totalTransactionValue;
+    },
+
+    getCurrentPrice(cryptoId) {
+      if (this.cryptoPricesObject[cryptoId]) {
+        return this.cryptoPricesObject[cryptoId].toFixed(2);
+      } else {
+        return 'Not Available';
+      }
+    },
+
+    calculatePriceChange(crypto) {
+      const currentPrice = this.getCurrentPrice(crypto.id);
+      if (currentPrice === 'Not Available') return '0';
+
+      const change = ((currentPrice - crypto.price) / crypto.price) * 100;
+      return change.toFixed(2);
+    },
+
+    getChangeColor(crypto) {
+    const changeColor = this.calculatePriceChange(crypto);
+      if (changeColor >= 0) {
+        return 'color: green;';
+      } else {
+        return 'color: red;';
+      }
+    },
+
+    getTotalWalletColor() {
+      if (this.totalWallet >= 0) {
+        return 'color: green;';
+      } else {
+        return 'color: red;';
+      }
+    },
+
+    addCrypto(newCrypto) {
+      this.userCryptoData.push(newCrypto);
+      this.walletService.saveUserCryptoData(this.userId, this.userCryptoData);
+      this.calculateTotalMarketValue();
+      this.calculateTotalTransactionValue();
+      this.calculateTotalWallet();
+    }
+  },
+  mounted() {
+    this.loadUserCryptoData();
+    this.updateCryptoPricesObject();
+    this.calculateTotalTransactionValue();
+    this.calculateTotalMarketValue();
+    this.calculateTotalWallet();
+  }
+
+};
 </script>
 
+
 <template>
-  <div>
-    <h1>Wallet</h1>
+  <div style="display: flex; flex-direction: column; align-items: center; color: white; min-height: 100vh; padding: 24px; margin-top: 50px;">
 
-    <!-- TODO: Mario - Add a form for the user to add a new crypto -->
-    <!-- The user should be able to select the crypto from a dropdown -->
-    <!-- The user should be able to add the amount of the crypto -->
-    <!-- The user should be able to add the price of the crypto -->
-    <!-- The user should be able to add the notes of the crypto -->
+    <div style="display: flex; flex-direction: row; justify-content: space-between; align-items: flex-start; width: 100%; max-width: 1200px; gap: 20px;">
 
-    <!-- TODO: Mario - Display the user crypto data -->
+      <div style="width: 40%; max-width: 450px;">
+        <WalletForm :cryptoPricesStore="cryptoPricesStore" @crypto-added="addCrypto" />
+      </div>
+
+      <div style="width: 55%; display: flex; flex-direction: column; gap: 20px;">
+        <div style="background-color: black; border-radius: 10px; border: 1px solid gray; text-align: center; padding: 16px;">
+          <p style="font-size: 20px; font-weight: bold;">Invested: ${{ totalTransactionValue }}</p>
+          <p style="font-size: 20px; font-weight: bold;">Current: ${{ totalMarketValue }}</p>
+          <p :style="getTotalWalletColor()" style="font-size: 20px; font-weight: bold;">Total Wallet: ${{ totalWallet }}</p>
+        </div>
+
+        <div>
+          <h2 class="text-lg mb-4">Transaction History</h2>
+          <table style="width: 100%; border-collapse: collapse; text-align: center; background-color: darkslategray;">
+            <thead>
+              <tr style="background-color: black; color: white;">
+                <th style="padding: 8px;">Cryptocurrency</th>
+                <th style="padding: 8px;">Amount</th>
+                <th style="padding: 8px;">Total (USD)</th>
+                <th style="padding: 8px;">Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="crypto in userCryptoData" :key="crypto.id" style="border-bottom: 1px solid gray;">
+                <td style="padding: 8px;">{{ crypto.name }} ({{ crypto.symbol }})</td>
+                <td style="padding: 8px;">{{ crypto.amount }}</td>
+                <td style="padding: 8px;">${{ (crypto.amount * crypto.price).toFixed(2) }}</td>
+                <td style="padding: 8px;">{{ crypto.date }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div>
+          <h2 class="text-lg mb-4">Current Value</h2>
+          <table style="width: 100%; border-collapse: collapse; text-align: center; background-color: darkslategray;">
+            <thead>
+              <tr style="background-color: black; color: white;">
+                <th style="padding: 8px;">Cryptocoin</th>
+                <th style="padding: 8px;">Updated</th>
+                <th style="padding: 8px;">Change (%)</th>
+                <th style="padding: 8px;">Price (USD)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="crypto in userCryptoData" :key="crypto.id" style="border-bottom: 1px solid gray;">
+                <td style="padding: 8px;">{{ crypto.name }} ({{ crypto.symbol }})</td>
+                <td style="padding: 8px;">{{ cryptoPricesStore.lastUpdated }}</td>
+                <td :style="getChangeColor(crypto)" style="padding: 8px;">{{ calculatePriceChange(crypto) }}%</td>
+                <td style="padding: 8px;">${{ getCurrentPrice(crypto.id) }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<style>
+</style>
